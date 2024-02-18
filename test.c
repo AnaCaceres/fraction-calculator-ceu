fcf767f870081b3e66536280a460e889e364ff72
Ana Cáceres <anadelorenzoCL@gmail.com>
2024-02-18 12:22:58 +0100

    Add test to show all stored fractions

--- test.c
+++ test.c
@@ -10,6 +10,7 @@
 void la_calculadora_deberia_almacenar_una_fraccion();
 void la_calculadora_deberia_eliminar_una_fraccion();
 void la_calculadora_deberia_mostrar_una_fraccion();
+void la_calculadora_deberia_mostrar_todas_las_fracciones();
 
 // Fraction calculator should store a fraction
 void la_calculadora_deberia_almacenar_una_fraccion() {
@@ -94,14 +95,48 @@ void la_calculadora_deberia_mostrar_una_fraccion(){
   char expected_output[] = "Introduce posicion (1-4): 3\n5/7";
   assert(strcmp(buffer, expected_output));
   
-  
+  printf("%s\n", expected_output);
   printf("Fracción mostrada correctamente\n\n");
+}
+
+// Fraction calculator should show all fractions
+void la_calculadora_deberia_mostrar_todas_las_fracciones(){
+  printf("La calculadora debería mostrar todas las fracciones: \n");
+
+  int numeradores[MAX_FRACCIONES] = {3, 1, 5, 2};
+  int denominadores[MAX_FRACCIONES] = {4, 9, 7, 3};
+  int nFracciones = 4;
+
+  int stdout_original = dup(STDOUT_FILENO);
+  FILE *file = fopen("output.txt", "w");
+  // Redirige stdout al archivo temporal
+  dup2(fileno(file), fileno(stdout));
+  fclose(file);
+
+  opcion4(numeradores, denominadores, nFracciones);
+
+  // Lee el contenido del archivo temporal
+  file = fopen("output.txt", "r");
+  char buffer[100];
+  fgets(buffer, sizeof(buffer), file);
+  fclose(file);
+  // Restaura stdout
+  remove("output.txt");
+  dup2(stdout_original, STDOUT_FILENO);
+  close(stdout_original);
+
+  char expected_output[] = "Posicion 1: 3/4\nPosicion 2: 1/9\nPosicion 3: 5/7\nPosicion 4: 2/3";
+  assert(strcmp(buffer, expected_output));
+  
+  printf("%s\n", expected_output);
+  printf("Fracciones mostradas correctamente\n\n");
 }
 
 int main() {
     la_calculadora_deberia_almacenar_una_fraccion();
     la_calculadora_deberia_eliminar_una_fraccion();
     la_calculadora_deberia_mostrar_una_fraccion();
+    la_calculadora_deberia_mostrar_todas_las_fracciones();
 
     return 0;
 }
