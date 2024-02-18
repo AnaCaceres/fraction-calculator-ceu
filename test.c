e7389845825bf25c7fa0c6a6f11e90b72aa77ac8
Ana Cáceres <anadelorenzoCL@gmail.com>
2024-02-18 11:51:25 +0100

    Add test for removing a fraction

--- test.c
+++ test.c
@@ -5,10 +5,11 @@
 #include "fracciones.h"
 
 void la_calculadora_deberia_almacenar_una_fraccion();
+void la_calculadora_deberia_eliminar_una_fraccion();
 
 // Fraction calculator should store a fraction
 void la_calculadora_deberia_almacenar_una_fraccion() {
-    printf("Test 1: \n");
+    printf("La calculadora debería almacenar una fracción: \n");
 
     int numeradores[MAX_FRACCIONES] = {3};
     int denominadores[MAX_FRACCIONES] = {4};
@@ -27,12 +28,38 @@ void la_calculadora_deberia_almacenar_una_fraccion() {
         assert(denominadores[i] == expected_denominadores[i]);
     }
     
-    printf("Fracción almacenada correctamente\n");
+    printf("Fracción almacenada correctamente\n\n");
     fclose(user_input);
+}
+
+// Fraction calculator should remove a fraction
+void la_calculadora_deberia_eliminar_una_fraccion(){
+  printf("La calculadora debería eliminar una fracción: \n");
+
+  int numeradores[MAX_FRACCIONES] = {3, 1, 5};
+  int denominadores[MAX_FRACCIONES] = {4, 9, 7};
+  int nFracciones = 3;
+  FILE *user_input = fopen("test_mocks/2_remove_a_fraction.txt", "r");
+
+  stdin = user_input;
+  opcion2(numeradores, denominadores, &nFracciones);
+  stdin = stdin;
+
+  assert(nFracciones == 2);
+  int expected_numeradores[2] = {3, 5};
+  int expected_denominadores[2] = {4, 7};
+  for (int i = 0; i < nFracciones; i++) {
+      assert(numeradores[i] == expected_numeradores[i]);
+      assert(denominadores[i] == expected_denominadores[i]);
+  }
+  
+  printf("Fracción eliminada correctamente\n\n");
+  fclose(user_input);
 }
 
 int main() {
     la_calculadora_deberia_almacenar_una_fraccion();
+    la_calculadora_deberia_eliminar_una_fraccion();
 
     return 0;
 }
